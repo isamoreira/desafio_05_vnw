4d2f7aa03264435290fb824bf37901a9413d59ca
Isabella Moreira <babellamoreira@gmail.com>
2022-06-27 16:23:56 -0300

    Create desafio_05_vnw.js

---
+++ desafio_05_vnw.js
@@ -0,0 +1,14 @@
+let itens =["Colher", "Garfo", "Faca", "Prato", "Copo"]
+console.log(itens)
+itens.unshift('Panela')
+console.log(itens)
+itens.pop()
+console.log(itens)
+itens.push('Guardanapo', 'Tábua')
+console.log(itens)
+itens.shift()
+console.log(itens)
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
